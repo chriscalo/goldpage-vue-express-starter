1b92057aa961476287495eb5f7a5512663950d84
Chris Calo <chris.calo@gmail.com>
2019-09-14 16:24:25 -0400

    comments

--- pages/foo/index.page.js
+++ pages/foo/index.page.js
@@ -2,8 +2,6 @@ import index from './index.vue';
 import { route } from "../../util";
 
 export default {
-  // FIXME: Possible to supply a relative route? Express subapps work this way
-  // and it makes composition (and refactoring) much simpler.
   route: route(__filename),
   view: index,
   addInitialProps() {
--- util/index.js
+++ util/index.js
@@ -1,6 +1,6 @@
 import path from "path";
 
-// converts *.page.js into route string
+// convert *.page.js file path into route string
 export function route(filename) {
   const route = String(filename)
     // web server routes are relative to pages dir
