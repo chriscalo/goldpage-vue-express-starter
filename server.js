c2237e604bd45ec7bdb3a328584d8c203d1fc9af
Chris Calo <chris.calo@gmail.com>
2019-10-02 23:04:05 -0400

    Move server.js to server/index.js

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { start } = require("express-start");
-const ssr = require("goldpage");
-
-const server = express();
-
-server.use(ssr.express);
-
-start(server);
