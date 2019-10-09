8122e3bc21a0b2e0a52d29757b382d21d5a48fa8
Chris Calo <chris.calo@gmail.com>
2019-10-08 22:05:00 -0400

    Simplify endpoints.greet

--- api/index.js
+++ api/index.js
@@ -2,11 +2,7 @@ import { endpoints } from "wildcard-api";
 import { file } from "ez-file";
 
 endpoints.greet = async function greet() {
-  // The getting started section of the [Wildcard API docs][wildcard-docs] state
-  // that `this` will be equal to the `requestProps` object passed to
-  // `getApiResponse()`.
-  //
-  // [wildcard-docs]: https://github.com/reframejs/wildcard-api
-  console.log((typeof window==='undefined'?'server':'browser')+'-side `requestProps`:', this);
+  // callers may pass a context object
+  const context = this;
   return await file("./greeting.txt");
 };
